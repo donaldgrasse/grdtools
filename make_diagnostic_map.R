a5445d9af4a3c378f86c49f2c7b02680d5101c70
donaldgrasse <95257807+donaldgrasse@users.noreply.github.com>
2024-03-10 19:51:57 -0700

    Update make_diagnostic_map.R

--- make_diagnostic_map.R
+++ make_diagnostic_map.R
@@ -3,35 +3,35 @@ make_diagnostic_map <- function(data, polygon, study_border, outcome, treat,
                                 lab_position, treat_poly, different_slope, 
                                 projection){
   
+  #Load required packages
   packs <- c('automap','BAMMtools', 'gstat', 'purrr', 'sf', 'sp', 'tmap')
   suppressMessages(lapply(packs, require, character.only = T))
-  
+
+  #define parameters
   outcome <- outcome
   treat <- treat
-  
   treat_poly <- treat_poly
   
   data <- data[ ,c(outcome, treat)]
   
-  projection <- "+proj=longlat +ellps=WGS84 +no_defs"
-  
+  #project data
+  projection <- projection
   polygon <- polygon %>% 
     st_transform(st_crs(projection))
   
   study_border <- study_border %>% 
     st_transform(st_crs(projection))
   
+  #create a grid to project predictions across space
   polygon_sp <- as_Spatial(polygon)
   grid <- makegrid(polygon_sp, cellsize = .003)
   grid <- SpatialPoints(grid, proj4string = CRS("+proj=longlat +ellps=WGS84 +no_defs"))
   grid <- grid[polygon_sp, ]
-  
-  
   grid <- st_as_sf(grid)
-  
   grid <- grid %>% 
-    st_transform(st_crs("+proj=longlat +ellps=WGS84 +no_defs"))
+    st_transform(st_crs(projection))
   
+  #define treatment and xy coordinates for projected grid
   grid$treat <- as.numeric(as.numeric(st_distance(grid$geometry, treat_poly$geometry))==0,1,0)
   grid$y <- as.numeric(map_chr(grid$geometry, 1))
   grid$x <- as.numeric(map_chr(grid$geometry, 2))
@@ -39,21 +39,21 @@ make_diagnostic_map <- function(data, polygon, study_border, outcome, treat,
   grid <- as_Spatial(grid)
   
   df_sp <- data %>% 
-    st_transform(st_crs("+proj=longlat +ellps=WGS84 +no_defs"))
+    st_transform(st_crs(projection))
   
   treat_poly <- treat_poly %>% 
-    st_transform(st_crs("+proj=longlat +ellps=WGS84 +no_defs"))
+    st_transform(st_crs(projection))
   
   colnames(df_sp)[1] <- 'outcome'
   df_sp$y <- as.numeric(map_chr(df_sp$geometry, 1))
   df_sp$x <- as.numeric(map_chr(df_sp$geometry, 2))
-  
-  
   df_sp <- as_Spatial(df_sp)
-  
+
+  #if the researcher DOES NOT wants the impact of lat-lon coords to vary across 
+  #treatment and control areas
   if(different_slope == FALSE){
   
-  
+  #fit variogram
   vario.fit = autofitVariogram(outcome~1+x+y,
                                df_sp,
                                model = c("Exp", "Sph", "Mat"),
@@ -63,9 +63,8 @@ make_diagnostic_map <- function(data, polygon, study_border, outcome, treat,
                                verbose = T)
   fit.v = vario.fit$var_model
   
-  
+  #get predictions across space
   heat = krige(outcome ~ 1, locations = df_sp, beta = mean(df_sp$outcome), newdata = grid, model = fit.v)
-  
   heat_2 <- st_as_sf(heat)
   heat_2 <- st_buffer(heat_2, .003, endCapStyle="SQUARE")
   
@@ -90,9 +89,11 @@ make_diagnostic_map <- function(data, polygon, study_border, outcome, treat,
   
   return(map)
   }
-  
+
+  #if the researcher DOES want the impact of lat-lon coords to vary across 
+  #treatment and control areas
   else{
-    
+    #fit the variogram 
     vario.fit = autofitVariogram(outcome ~ treat + x+y+x:y,
                                  df_sp,
                                  model = c("Exp", "Sph", "Mat"),
@@ -100,16 +101,21 @@ make_diagnostic_map <- function(data, polygon, study_border, outcome, treat,
                                  fix.values = c(NA),
                                  start_vals = c(NA),
                                  verbose = T)
+    #extract the results
     fit.v = vario.fit$var_model
+
+    #get predictions across space
     g <- gstat(NULL, 'outcome', outcome~treat*(x+y+x:y), df_sp, model = fit.v)
-    
     heat = predict(g, 
                  locations = df_sp, newdata = grid, model = fit.v)
     
     heat_2 <- st_as_sf(heat)
     heat_2 <- st_buffer(heat_2, .004, endCapStyle="SQUARE")
     data <- st_as_sf(df_sp)
-    #return(heat_2)
+
+    jenks_breaks = getJenksBreaks(data$outcome, n_breaks)
+
+    #make the map
     map <- tm_shape(heat_2) + tm_fill('outcome.pred', title = legend_title,
                                       style = "cont", palette = 'BuPu'
     ) + 
